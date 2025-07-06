5cbcfc6ce995a791de7bc6928187274d89a7bd45
Soham Mondal <sohammondal1006@gmail.com>
2025-07-06 20:47:28 +0530

    Add files via upload

---
+++ Includefile/navbar.landing-2.php
@@ -0,0 +1,19 @@
+  <!-- nav bar -->
+    <nav>
+        <div class="container main-nav flex">
+            <a href="#" class="company-logo">
+                <img src="./images/Logo2.png" alt="company logo">
+            </a>
+            <div class="nav-links" id="nav-links">
+                <ul class="flex">
+                    <li><a href="#" class="hover-link"><b>My Profile</b></a></li>
+                    <li><a href="#" class="hover-link"><b>Templates</b></a></li>
+                    <li><a href="#" class="hover-link"><b>Downloads</b></a></li>
+                    <li><a href="actions/logout.action.php" class="hover-link primary-button">Logout</a></li>
+                </ul>
+            </div>
+            <a href="#" class="nav-toggle hover-link" id="nav-toggle">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+        </div>
+    </nav>
